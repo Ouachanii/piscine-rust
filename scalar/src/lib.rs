c4fde8b4f3b0ac454098a476f69a5c2cb2f2a8d3
abouachani <abderrahim.ouachani@ump.ac.ma>
2025-09-04 20:37:57 +0100

    first exercice

--- scalar/src/lib.rs
+++ scalar/src/lib.rs
@@ -1,25 +1,27 @@
 pub fn sum(left: u8, right: u8) -> u8 {
-    left + right
+    left.checked_add(right).unwrap()
 }
 
 pub fn diff(x: i16, y: i16) -> i16 {
-    x - y
+    x.checked_sub(y).unwrap()
 }
 
-pub fn pro(x: i8, y: i8) -> i16 {
-    (x as i16) * (y as i16)
+pub fn pro(x: i8, y: i8) -> i8 {
+    x.checked_mul(y).unwrap()
 }
 
 pub fn quo(x: f32, y: f32) -> f32 {
     if y != 0.0 {
-        return x / y;
+        x / y
+    } else {
+        0.0
     }
-    0.0
 }
 
 pub fn rem(x: f32, y: f32) -> f32 {
     if y != 0.0 {
-        return x % y;
+        x % y
+    } else {
+        0.0
     }
-    0.0
 }
