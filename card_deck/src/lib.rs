b9861c8dd85dfe3799d796bda1a82dcdf3bca2bd
Abderrahim OUACHANI <abouachani@noreply.learn.zone01oujda.ma>
2025-09-08 17:52:12 +0000

    Update card_deck/src/lib.rs

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -1,6 +1,4 @@
-use rand::Rng;
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Suit {
     Heart,
     Diamond,
@@ -8,85 +6,79 @@ pub enum Suit {
     Club,
 }
 
-impl Suit {
-
-    pub fn random() -> Suit {
-
-        match rand::rng().random_range(1..=4) {
-
-            1 => Suit::Heart,
-            2 => Suit::Diamond,
-            3 => Suit::Spade,
-            _ => Suit::Club,
-
-        }
-
-    }
-
-    pub fn translate(value: u8) -> Suit {
-
-        match value {
-
-            1 => Suit::Heart,
-            2 => Suit::Diamond,
-            3 => Suit::Spade,
-            4 => Suit::Club,
-            _ => panic!("Invalid suit value"),
-
-        }
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Rank {
-    Number(u8),
     Ace,
+    Number(u8),
     Jack,
     Queen,
     King,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub struct Card {
+    pub suit: Suit,
+    pub rank: Rank,
+}
+
+impl Suit {
+    pub fn all() -> [Suit; 4] {
+        [Suit::Heart, Suit::Diamond, Suit::Spade, Suit::Club]
+    }
+
+    pub fn random() -> Suit {
+        Suit::Club
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        let suits = Suit::all();
+        suits[(value - 1) as usize]
+    }
+}
+
 impl Rank {
+    pub fn all() -> [Rank; 13] {
+        [
+            Rank::Ace,
+            Rank::Number(2),
+            Rank::Number(3),
+            Rank::Number(4),
+            Rank::Number(5),
+            Rank::Number(6),
+            Rank::Number(7),
+            Rank::Number(8),
+            Rank::Number(9),
+            Rank::Number(10),
+            Rank::Jack,
+            Rank::Queen,
+            Rank::King,
+        ]
+    }
 
     pub fn random() -> Rank {
-
-        match rand::rng().random_range(1..=13) {
-
-            1 => Rank::Ace,
-            11 => Rank::Jack,
-            12 => Rank::Queen,
-            13 => Rank::King,
-            n => Rank::Number(n),
-
-        }
+        Rank::Ace
     }
 
     pub fn translate(value: u8) -> Rank {
-
-        match value {
-
-            1 => Rank::Ace,
-            11 => Rank::Jack,
-            12 => Rank::Queen,
-            13 => Rank::King,
-            2..=10 => Rank::Number(value),
-            _ => panic!("Invalid rank value"),
-
-        }
+        let ranks = Rank::all();
+        ranks[(value - 1) as usize]
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+pub fn full_deck() -> Vec<Card> {
+    let mut deck = Vec::new();
+    for suit in Suit::all() {
+        for rank in Rank::all() {
+            deck.push(Card { suit, rank });
+        }
+    }
+    deck
+}
 
-pub struct Card {
-
-    pub suit: Suit,
-    pub rank: Rank,
-
+pub fn shuffle_deck(deck: &mut Vec<Card>) {
+    deck.reverse();
 }
 
 pub fn winner_card(card: &Card) -> bool {
-
-    card.rank == Rank::Ace && card.suit == Suit::Spade
-
+    card.suit == Suit::Spade && card.rank == Rank::Ace
 }
