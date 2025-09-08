0be5112ef7462e5981ff9fa9b0d0602bf8e35610
abouachani <abderrahim.ouachani@ump.ac.ma>
2025-09-08 20:01:58 +0100

    q3 ex12

---
+++ collect/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn bubble_sort(arr: &mut [i32]) {
+
+    let n = arr.len();
+    
+    if n <= 1 {
+        return;
+    }
+
+    for i in 0..n {
+
+        for j in 0..n - i - 1 {
+
+            if arr[j] > arr[j + 1] {
+
+                arr.swap(j, j + 1);
+
+            }
+
+        }
+
+    }
+}
