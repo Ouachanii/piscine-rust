33dfb0e80958ce9da183f45e9e9e5863b92060e0
abouachani <abderrahim.ouachani@ump.ac.ma>
2025-09-11 17:45:24 +0100

    q4 ex3

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,9 +1,13 @@
+use std::fs::OpenOptions;
+use std::io::Write;
 use std::path::Path;
 
 pub fn open_or_create<P: AsRef<Path>>(path: &P, content: &str) {
-    if let Ok(p) = std::fs::read_to_string(path) {
-        println!("File content: {}", p);
-    } else {
-        std::fs::write(path, content).unwrap();
-    }
+    let mut file = OpenOptions::new()
+        .create(true)
+        .append(true)
+        .open(path)
+        .unwrap();
+
+    file.write_all(content.as_bytes()).unwrap();
 }
